27d7f745c02a6e2a0387c2a1c2af7f4c4e8531f6
Himanshu0422 <himanshumittal035@gmail.com>
2023-08-03 15:35:11 +0530

    cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 require("dotenv").config();
 const dbConfig = require('./config/dbConfig');
