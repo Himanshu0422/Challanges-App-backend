e2aca02f764c89de399b3a1f2eed64193a043d11
Himanshu0422 <himanshumittal035@gmail.com>
2023-08-03 15:57:17 +0530

    server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://challanges-app.vercel.app' }));
 app.use(express.json());
 require("dotenv").config();
 const dbConfig = require('./config/dbConfig');
